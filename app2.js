75e4a97758dab31a0b57157b4b71f819d0d470a8
jenjadraque <46984834+jenjadraque@users.noreply.github.com>
2019-01-31 15:01:29 +0800

    uploaded app2.js

---
+++ app2.js
@@ -0,0 +1,58 @@
+// Set constraints for the video stream
+var view1  = "user";
+var view2 = "environment";
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    rearcameraTrigger = document.querySelector("#rearcamera--trigger")
+
+//Identify 2 views, Front Camera shooting and Rear Camera Shooting
+var constraints = {
+	video:{
+		facingMode: view1
+	},
+	audio: false
+}
+var rearconstraints = {
+	video:{
+		facingMode: view2
+	},
+	audio: false
+}
+
+// Access the device camera and stream to cameraView (frontview)
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+//rearcamera
+function rearcameraStart(){
+	navigator.mediaDevices.getUserMedia(rearconstraints).then(function(stream){
+		track = stream.getTracks()[0];
+		cameraView.srcObject = stream;
+	}).catch(function(error){
+		console.error("Error. Rear Camera not found.");
+	});
+}
+
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
