94efc19efd0f88465cb560b0a3ab199ed24f8162
jenjadraque <46984834+jenjadraque@users.noreply.github.com>
2019-01-31 10:45:37 +0800

    Updated
    updated to include toggle camera view from self to environment

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-﻿// Set constraints for the video stream
+// Set constraints for the video stream
 var view1  = "user";
 var view2 = "environment";
 
-var constraints = { video: { facingMode: view2 }, audio: false };
+/*var constraints = {
+    video: { facingMode: view1 }, 
+    audio: false 
+}; */
+
+//toggle camera views
+var constraints ={
+        video:{facingMode: view1},
+        audio: false
+    } 
+
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
@@ -10,42 +20,35 @@ const cameraView = document.querySelector("#camera--view"),
     cameraTrigger = document.querySelector("#camera--trigger"),
     rearcameraTrigger = document.querySelector("#rearcamera--trigger")
 
-rearcameraTrigger.onclick = function () {
-    alert("Changing constraints.");
-    var constraints = {
-        video: {
-            facingMode: view1,
-            audio: false
-        }
-    };
-    console.log(constraints);
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-}
-
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function (stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-    .catch(function (error) {
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
         console.error("Oops. Something is broken.", error);
     });
 }
-
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function () {
+cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+};
+
+rearcameraTrigger.onclick= function(){
+    constraints = {
+            video:{
+                facingMode: view2
+            },
+            audio: false
+        }
+        console.log(constraints);
 };
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
